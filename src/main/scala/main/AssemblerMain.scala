9938307004f071275880f530ccfbfcd26ebfa9d5
Vlad Shiligin <vlad.shiligin@ons.gov.uk>
2018-01-30 22:25:54 +0000

    code used to create parquet file commented out

--- src/main/scala/main/AssemblerMain.scala
+++ src/main/scala/main/AssemblerMain.scala
@@ -4,7 +4,7 @@ package main
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
   *
@@ -12,19 +12,20 @@ import org.apache.spark.sql.SparkSession
 object AssemblerMain extends App{
 
   override def main(args: Array[String]) {
-    val spark = SparkSession
+    val spark: SparkSession = SparkSession
       .builder()
-      .master("local[8]")
+      .master("local[4]")
       .appName("Spark SQL basic example")
       .config("spark.ui.port", "18080")
       .getOrCreate()
-    import spark.implicits._
-/*    val data = spark.read.json("src/main/resources/sample.json")///Users/VLAD/dev/ons/enterprise-assemble/src/main/resources/sample.csv
-    data.printSchema()
+  //import spark.implicits._
+   val data = spark.read.json("src/main/resources/sample.json")///Users/VLAD/dev/ons/enterprise-assemble/src/main/resources/sample.csv
+  /* data.printSchema()
     data.write.parquet("src/main/resources/sample.parquet")*/
-    val parquetFileDF = spark.read.parquet("src/main/resources/sample.parquet")
+    val parquetFileDF: DataFrame = spark.read.parquet("src/main/resources/sample.parquet")
     parquetFileDF.createOrReplaceTempView("businessIndexRec")
     val namesDF = spark.sql("SELECT BusinessName FROM businessIndexRec WHERE BusinessName LIKE '%TEST%'")
     namesDF.show()
+    //parquetFileDF.rdd.saveAsNewAPIHadoopFile
   }
 }
