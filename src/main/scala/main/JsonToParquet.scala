1ac05f8227c62f3487d96ebb0a40218c0d38558e
Vlad Shiligin <vlad.shiligin@ons.gov.uk>
2018-02-01 20:49:12 +0000

    JsonToParquet: made to work

--- src/main/scala/main/JsonToParquet.scala
+++ src/main/scala/main/JsonToParquet.scala
@@ -1,17 +1,13 @@
 package main
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
-
 /**
   *
   */
-class JsonToParquet {
-  val spark: SparkSession = SparkSession
-    .builder()
-    .master("local[4]")
-    .appName("enterprise assembler")
-    .config("spark.ui.port", "18080")
-    .getOrCreate()
-  val data: DataFrame = spark.read.json("src/main/resources/sample.json")
-  data.write.parquet("src/main/resources/sample.parquet")
+object JsonToParquet {
+
+  def convertAndSave(spark: SparkSession) {
+    val data: DataFrame = spark.read.json("src/main/resources/sample.json")
+    data.write.parquet("src/main/resources/sample.parquet")
+  }
 }
