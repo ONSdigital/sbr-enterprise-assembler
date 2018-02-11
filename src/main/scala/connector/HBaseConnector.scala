6123c9c301f3576c50bb8d2d86060e32f143e1c2
tigdik <vlad.shiligin@ons.gov.uk>
2018-02-11 15:23:04 +0000

    redundant comments removed

--- src/main/scala/connector/HBaseConnector.scala
+++ src/main/scala/connector/HBaseConnector.scala
@@ -21,21 +21,6 @@ object HBaseConnector {
 
   val logger = LoggerFactory.getLogger(getClass)
 
-/*  def getIntValue(resourceKey:String, defaultVal:Int) = Try{config.getInt(resourceKey)}.getOrElse(defaultVal)
-  def getStrValue(resourceKey:String, defaultVal:String): String = Try{config.getString(resourceKey)}.getOrElse(defaultVal)
-
-
-  val conf: Configuration = HBaseConfiguration.create()
-  Try{config.getString("hbase.kerberos.config")}.map(conf.addResource).getOrElse(logger.info("no config resource for kerberos specified"))
-  Try{config.getString("hbase.local.path.config")}.map(conf.addResource).getOrElse {
-                logger.info("no config resource for hbase specified. Default configs will be used")
-                conf.set("hbase.zookeeper.quorum", config.getString("hbase.local.zookeper.url"))
-                conf.setInt("hbase.mapreduce.bulkload.max.hfiles.perRegion.perFamily", config.getInt("hbase.local.files.per.region"))
-             }*/
-
-
-  //val connection: Connection = ConnectionFactory.createConnection(conf)
-
 
   private def setJob(table:Table)(implicit connection:Connection) = {
     val job = Job.getInstance(connection.getConfiguration)
@@ -54,37 +39,4 @@ object HBaseConnector {
     table.close
     admin.close
   }
-
-
-
-  /*def closeConnection = if(connectionClosed) Unit else System.exit(1)
-
-
-  private def connectionClosed: Boolean = {
-
-    def isClosed(waitingMillis: Long): Boolean = if (!connection.isClosed) {
-      wait(waitingMillis)
-      connection.isClosed
-    } else true
-
-    @tailrec
-    def tryClosing(checkIntervalSec: Long, totalNoOfAttempts: Int, noOfAttemptsLeft: Int): Boolean = {
-      if (connection.isClosed) true
-      else if (noOfAttemptsLeft == 0) {
-        logger.warn(s"Could not close HBase connection. Attempted $totalNoOfAttempts times with intervals of $checkIntervalSec millis")
-        false
-      } else {
-        connection.close
-        if (isClosed(checkIntervalSec)) true
-        else {
-          logger.info(s"trying closing hbase connection. Attempt ${totalNoOfAttempts - noOfAttemptsLeft} of $totalNoOfAttempts")
-          tryClosing(checkIntervalSec, totalNoOfAttempts, noOfAttemptsLeft - 1)
-        }
-      }
-    }
-
-
-    tryClosing(1000L, 5, 5)
-  }
-*/
 }
