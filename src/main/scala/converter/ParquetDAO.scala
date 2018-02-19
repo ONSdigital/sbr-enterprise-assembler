4cb6bab72a1def99f041c9db8c183611458110f1
tigdik <vlad.shiligin@ons.gov.uk>
2018-02-19 12:00:03 +0000

    added logging to ParquetDao

--- src/main/scala/converter/ParquetDAO.scala
+++ src/main/scala/converter/ParquetDAO.scala
@@ -2,11 +2,14 @@ package converter
 
 
 
+import connector.HBaseConnector.getClass
+import converter.ParquetDAO.logger
 import global.Configured
 import org.apache.hadoop.hbase.KeyValue
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.slf4j.LoggerFactory
 
 
 
@@ -15,6 +18,8 @@ case class RowObject(key:String, colFamily:String, qualifier:String, value:Strin
 }
 
 object ParquetDAO extends WithConversionHelper{
+
+  val logger = LoggerFactory.getLogger(getClass)
 
   def jsonToParquet(jsonFilePath:String, parquetFilePath:String)(implicit spark:SparkSession):Unit = {
     val data: DataFrame = spark.read.json(jsonFilePath)
@@ -26,7 +31,9 @@ object ParquetDAO extends WithConversionHelper{
 
     val data = parquetFileDF.rdd.flatMap(rowToEnt).sortBy(t => s"${t._2.key}${t._2.qualifier}")
 
+    logger.debug("Start saving HFILE to HDFS...")
     data.map(rec => (new ImmutableBytesWritable(rec._1.getBytes()), rec._2.toKeyValue)).saveAsNewAPIHadoopFile(pathToHFile,classOf[ImmutableBytesWritable],classOf[KeyValue],classOf[HFileOutputFormat2],Configured.conf)
+    logger.debug("Finished saving HFILE to HDFS...")
     data.unpersist()
   }
 }
