ae114ccbef31f237c6b66f7c93486e96aab20fa9
tigdik <vlad.shiligin@ons.gov.uk>
2018-02-20 17:31:20 +0000

    style

--- src/main/scala/global/Configs.scala
+++ src/main/scala/global/Configs.scala
@@ -57,9 +57,11 @@ object Configs{
     Try(args(0)).map(conf.set("hbase.table.links.name", _)).getOrElse(Unit)
     Try(args(1)).map(conf.set("hbase.table.links.namespace", _)).getOrElse(Unit)
     Try(args(2)).map(conf.set("files.links.hfile", _)).getOrElse(Unit)
+
     Try(args(3)).map(conf.set("hbase.table.enterprise.name", _)).getOrElse(Unit)
     Try(args(4)).map(conf.set("hbase.table.enterprise.namespace", _)).getOrElse(Unit)
     Try(args(5)).map(conf.set("files.enterprise.hfile", _)).getOrElse(Unit)
+
     Try(args(6)).map(conf.set("files.parquet", _)).getOrElse(Unit)
     Try(args(7)).map(conf.set("hbase.zookeeper.quorum", _)).getOrElse(Unit)
     Try(args(8)).map(conf.set("hbase.zookeeper.property.clientPort", _)).getOrElse(Unit)
