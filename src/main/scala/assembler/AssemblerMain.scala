6e2ac117401f9893f8b3d22e215f1300a6709a82
ranjit-das <Ranjit.Das@ons.gov.uk>
2018-03-20 16:22:59 +0000

    zookeeper params added

--- src/main/scala/assembler/AssemblerMain.scala
+++ src/main/scala/assembler/AssemblerMain.scala
@@ -1,16 +1,19 @@
 package assembler
 
 
-import global.AppParams
+import global.{AppParams, Configs}
 import service.EnterpriseAssemblerService
 
 
 object AssemblerMain extends EnterpriseAssemblerService {
 
   def main(args: Array[String]) {
+    Configs.conf.set("hbase.zookeeper.quorum", args(9))
+    Configs.conf.set("hbase.zookeeper.property.clientPort", args(10))
+    //Configs.conf.set(AppParams(9),AppParams(10))
      //refreshFromParquet(AppParams(args))
-     loadFromParquet(AppParams(args))
-     //loadFromJson(AppParams(args))
+     //loadFromParquet(AppParams(args))
+     loadFromJson(AppParams(args))
     //loadFromHFile(AppParams(args))
 
   }
