f165606b9fe68d1274897edc27d40737808c666b
vladshiligin <vladtoldmetodoit@hotmail.com>
2018-03-21 15:16:42 +0000

    tests refactored

--- src/test/scala/dao/HFileTestUtils.scala
+++ src/test/scala/dao/HFileTestUtils.scala
@@ -10,30 +10,31 @@ trait HFileTestUtils {
 
    def assignStaticKeys(rows:Seq[HFileRow]): Set[HFileRow] = {
 
-    val erns: Seq[(String, Int)] = rows.collect{case row if(row.cells.find(_.column=="p_ENT").isDefined) => {row.cells.collect{case HFileCell("p_ENT",value) => value}}}.flatten.zipWithIndex
 
-    val ernsDictionary: Seq[(String, String)] = erns.map(ernTup => {
-                                                              val (ern,index) = ernTup
-                                                              (ern,"testEnterpriseId-"+({index+1}.toString*5))
+    //dictionary mapping actual erns to static
+    val ernsDictionary: Seq[(String, String)] = {
 
-                                                              })
+      val erns: Seq[(String, Int)] = rows.collect{case row if(row.cells.find(_.column=="p_ENT").isDefined) => {row.cells.collect{case HFileCell("p_ENT",value) => value}}}.flatten.zipWithIndex
 
-                //replace erns in actual:
-    rows.map {  case row  => {
-                            if (ernsDictionary.find(_._1 == row.key.slice(0, 18)).isDefined) {
-                            val ern = ernsDictionary.find(_._1 == row.key.slice(0, 18))
-                            val rr: HFileRow = HFileRow(ern.get._2, row.cells)
-                            rr
+      erns.map(ernTup => {
+        val (ern,index) = ernTup
+        (ern,"testEnterpriseId-"+({index+1}.toString*5))
 
-                          }else if(row.cells.find(cell => cell.column=="p_ENT").isDefined) {
-                            row.copy(row.key, row.cells.map {cell =>
-                            if (cell.column == "p_ENT"){
-                            val value = ernsDictionary.find(_._1 == cell.value)
-                            cell.copy(value = value.get._2)
-                          }else cell
-                          })
-                          }else row
-              }}.toSet
+        })}
+
+    //replace erns in rows:
+    rows.map {  case row  => if (ernsDictionary.find(_._1 == row.key.slice(0, 18)).isDefined) {
+                                  val ern = ernsDictionary.find(_._1 == row.key.slice(0, 18))
+                                  HFileRow(ern.get._2, row.cells)
+                                }else if(row.cells.find(cell => cell.column=="p_ENT").isDefined) {
+                                        row.copy(row.key, row.cells.map {cell =>
+                                        if (cell.column == "p_ENT"){
+                                            val value = ernsDictionary.find(_._1 == cell.value)
+                                            cell.copy(value = value.get._2)
+                                        }else cell
+                                      })
+                                }else row
+              }.toSet
    }
 
 }
