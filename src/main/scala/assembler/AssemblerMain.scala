4b301ebb683ff836e00956051b411764707589e5
vladshiligin <vladtoldmetodoit@hotmail.com>
2018-04-03 12:42:29 +0100

    logging added

--- src/main/scala/assembler/AssemblerMain.scala
+++ src/main/scala/assembler/AssemblerMain.scala
@@ -1,17 +1,62 @@
 package assembler
 
 
+import dao.hbase.HBaseDao.withScanner
+import executors.RefreshClosures.readDeleteData
 import global.{AppParams, Configs}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.client.Scan
+import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp
+import org.apache.hadoop.hbase.filter.{RegexStringComparator, RowFilter}
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.protobuf.ProtobufUtil
+import org.apache.hadoop.hbase.protobuf.generated.ClientProtos
+import org.apache.hadoop.hbase.util.Base64
+import org.apache.spark.sql.SparkSession
 import service._
+import spark.extensions.rdd.HBaseDataReader
 
 
 object AssemblerMain extends EnterpriseAssemblerService with EnterpriseRefreshService{
 
+  private def unsetScanner(config:Configuration) = config.unset(TableInputFormat.SCAN)
+
+  private def setScanner(config:Configuration,regex:String, appParams:AppParams) = {
+
+    val comparator = new RegexStringComparator(regex)
+    val filter = new RowFilter(CompareOp.EQUAL, comparator)
+
+    def convertScanToString(scan: Scan): String = {
+      val proto: ClientProtos.Scan = ProtobufUtil.toScan(scan)
+      return Base64.encodeBytes(proto.toByteArray())
+    }
+
+    val scan = new Scan()
+    scan.setFilter(filter)
+    val scanStr = convertScanToString(scan)
+
+    config.set(TableInputFormat.SCAN,scanStr)
+  }
+
   def main(args: Array[String]) {
     Configs.conf.set("hbase.zookeeper.quorum", args(9))
     Configs.conf.set("hbase.zookeeper.property.clientPort", args(10))
-    val appParams = args.take(9)++args.takeRight(2)
-    printDeleteData(AppParams(appParams))
+    val appParams = AppParams(args.take(9)++args.takeRight(2))
+    withSpark{ implicit ss:SparkSession =>
+      val tableName = s"${appParams.HBASE_LINKS_TABLE_NAMESPACE}:${appParams.HBASE_LINKS_TABLE_NAME}"
+      Configs.conf.set(TableInputFormat.INPUT_TABLE, tableName)
+      val regex = ".*(?<!~ENT~"+{appParams.TIME_PERIOD}+")$"
+      setScanner(Configs.conf,regex,appParams)
+      val resRdd = HBaseDataReader.readKvsFromHBase(Configs.conf)
+      unsetScanner(Configs.conf)
+      resRdd.take(5).map(_.toString).foreach(row => print(
+      "="*10+
+        row+'\n'+
+        "="*10
+    ))
+
+    }
+    //printDeleteData(AppParams(appParams))
     //loadRefresh(AppParams(appParams))
 
      //loadFromParquet(AppParams(appParams))
