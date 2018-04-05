a523b4f6102af9a6585b0773c21361c1e778ef07
vladshiligin <vlad.shiligin@ons.gov.uk>
2018-04-05 10:33:34 +0100

    with CreateClosure now

--- src/main/scala/service/EnterpriseAssemblerService.scala
+++ src/main/scala/service/EnterpriseAssemblerService.scala
@@ -2,31 +2,31 @@ package service
 
 import dao.hbase.{HBaseConnectionManager, HBaseDao}
 import dao.parquet.ParquetDAO
+import closures.CreateClosures
 import global.AppParams
 import org.apache.hadoop.hbase.client.Connection
 import org.apache.spark.sql.SparkSession
 import spark.SparkSessionManager
-import executors.CreateClosures._
 /**
   *
   */
-trait EnterpriseAssemblerService extends HBaseConnectionManager with SparkSessionManager{
+trait EnterpriseAssemblerService extends HBaseConnectionManager with SparkSessionManager with CreateClosures{
   import global.Configs._
 
 
 
-  def loadFromJson(appconf:AppParams)(implicit ss:SparkSession){
+  def loadFromJson(appconf:AppParams) = {  withSpark { implicit ss: SparkSession =>
 
-     // ParquetDAO.jsonToParquet(PATH_TO_JSON)(ss,appconf)
-      ParquetDAO.parquetToHFile(ss,appconf)
-
+                                             ParquetDAO.jsonToParquet(PATH_TO_JSON)(ss, appconf)
+                                             ParquetDAO.parquetToHFile(ss, appconf)
+                                           }
     withHbaseConnection { implicit con: Connection => HBaseDao.loadHFiles(con,appconf)}
   }
 
 
   def loadFromParquet(appconf:AppParams){
     withSpark{ implicit ss:SparkSession =>
-    withHbaseConnection { implicit con: Connection => loadFromParquet(appconf) }
+    withHbaseConnection { implicit con: Connection => loadFromCreateParquet(appconf) }
   }}
 
   def loadFromHFile(appconf:AppParams) = withHbaseConnection { implicit con: Connection => HBaseDao.loadHFiles(con,appconf)}
