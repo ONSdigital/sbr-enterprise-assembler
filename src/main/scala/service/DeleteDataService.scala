a5e204587e2ebfcfed031fb326fba7b91a68b70d
vladshiligin <vlad.shiligin@ons.gov.uk>
2018-05-09 10:39:24 +0100

    executing HFiles logic added to deletePeriod method

--- src/main/scala/service/DeleteDataService.scala
+++ src/main/scala/service/DeleteDataService.scala
@@ -1,7 +1,7 @@
 package service
 
 import closures.DeleteClosures
-import dao.hbase.HBaseConnectionManager
+import dao.hbase.{HBaseConnectionManager, HBaseDao}
 import global.AppParams
 import org.apache.hadoop.hbase.client.Connection
 import org.apache.spark.sql.SparkSession
@@ -12,7 +12,7 @@ trait DeleteDataService extends HBaseConnectionManager with SparkSessionManager{
   def deletePeriod(appconf:AppParams) = withSpark(appconf){ implicit ss:SparkSession =>
     withHbaseConnection{implicit con:Connection =>
       DeleteClosures.deleteDataForPeriod(appconf)
-
+      HBaseDao.loadHFiles(con,appconf)
     }
   }
 }
