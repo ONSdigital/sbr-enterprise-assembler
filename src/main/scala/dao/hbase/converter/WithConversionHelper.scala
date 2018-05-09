a822c5bc9ff4bd82a7d6a9b65ab71dbb5572a063
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-05-09 14:36:31 +0100

    Added vat vars to ent creation

--- src/main/scala/dao/hbase/converter/WithConversionHelper.scala
+++ src/main/scala/dao/hbase/converter/WithConversionHelper.scala
@@ -82,7 +82,11 @@ trait WithConversionHelper {
       row.getString("PostCode") map (pc => createEnterpriseCell(ern, "postcode", pc, appParams)),
       row.getString("LegalStatus").map(ls => createEnterpriseCell(ern, "legalstatus", ls, appParams)),
       row.getCalcValue("paye_employees").map(employees => createEnterpriseCell(ern, "paye_employees", employees, appParams)),
-      row.getCalcValue("paye_jobs").map(jobs => createEnterpriseCell(ern, "paye_jobs", jobs, appParams))
+      row.getCalcValue("paye_jobs").map(jobs => createEnterpriseCell(ern, "paye_jobs", jobs, appParams)),
+      row.getCalcValue("apportion_turnover").map(apportion => createEnterpriseCell(ern, "apportion_turnover", apportion, appParams)),
+      row.getCalcValue("total_turnover").map(total => createEnterpriseCell(ern, "total_turnover", total, appParams)),
+      row.getCalcValue("temp_contained_rep_vat_turnover").map(contained => createEnterpriseCell(ern, "temp_contained_rep_vat_turnover", contained, appParams)),
+      row.getCalcValue("temp_standard_vat_turnover").map(standard => createEnterpriseCell(ern, "temp_standard_vat_turnover", standard, appParams))
     ).collect { case Some(v) => v }
 
 /**/
