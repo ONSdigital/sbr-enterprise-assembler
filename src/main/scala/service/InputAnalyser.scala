d162050d637d16773b6a269d70e809da2da9fd66
vladshiligin <vladtoldmetodoit@hotmail.com>
2018-06-01 16:17:12 +0100

    invalid data scrip fixed and InputAnalyser fixed wrong childless Enterprises reporting

--- src/main/scala/service/InputAnalyser.scala
+++ src/main/scala/service/InputAnalyser.scala
@@ -15,19 +15,19 @@ object InputAnalyser extends RddLogging{
 
   def getData(appconf:AppParams)(implicit spark: SparkSession):DataReport =  {
 
-    val entRdd: RDD[HFileRow] = getRepartionedRdd(HBaseDao.readEnterprisesWithKeyFilter(Configs.conf,appconf, ".*~"+{appconf.TIME_PERIOD}+"$"))
+    val entRdd: RDD[HFileRow] = getRepartionedRdd(getEntsFromEntTable(appconf))
+    //printRdd("ENT RDD",entRdd,"HFileRow")
     entRdd.cache()
 
 
 
     //printRdd("enterprises",entRdd,"HFileRow")
-    val lusRdd: RDD[HFileRow] = getRepartionedRdd(HBaseDao.readLinksWithKeyFilter(Configs.conf,appconf, ".*~LEU~"+{appconf.TIME_PERIOD}+"$")
-                                                 .collect{ case row if(row.cells.find(_.column == "p_ENT").isDefined) => row })
-
+    val lusRdd: RDD[HFileRow] = getRepartionedRdd(getLegalUnitsFromLinks(appconf))
+    lusRdd.cache()
     //printRdd("LEU",lusRdd,"HFileRow")
 
-    val losRdd: RDD[HFileRow] = getRepartionedRdd(HBaseDao.readLouWithKeyFilter(Configs.conf,appconf, ".*~"+{appconf.TIME_PERIOD}+"~.*"))
-
+    val losRdd: RDD[HFileRow] = getRepartionedRdd(getLocalUnitsFromLouTable(appconf))
+    losRdd.cache()
 
     //printRdd("LOU",losRdd,"HFileRow")
 
@@ -35,35 +35,60 @@ object InputAnalyser extends RddLogging{
     entErns.cache()
     val entsWithKeyDiscrepancies: RDD[(String, String)] = entRdd.collect{case row if(row.key.split("~").head.reverse != row.cells.find(_.column=="ern").get.value) => (row.key,row.cells.find(_.column=="ern").get.value)}
     //printRdd("entsWithKeyDiscrepancies",entRdd,"(String,String)")
-    val entCount = entRdd.count()
+
 
     val luErns: RDD[String] = getRepartionedRdd(lusRdd.map(row => row.cells.find(_.column == "p_ENT").get.value).distinct())
+    luErns.cache()
     val loErns: RDD[String] = getRepartionedRdd(losRdd.map(row => row.cells.find(_.column == "ern").get.value).distinct())
-
+    loErns.cache()
     val luIntersection: RDD[String] = entErns.intersection(luErns)
     val loIntersection: RDD[String] = entErns.intersection(loErns)
     val orphanLus: RDD[(String, (String, String))] = getOrphanLus(lusRdd,luIntersection)
     val orphanLos: RDD[(String, (String, String))] = getOrphanLos(losRdd,loIntersection)
     val childlessEnts = getChildlessEnts(entErns,luErns,loErns)
 
-
+    val entCount = entRdd.count()
     val res = DataReport(entCount,lusRdd.count(),losRdd.count(),childlessEnts.collect(), entsWithKeyDiscrepancies.collect(),orphanLus.collect(),orphanLos.collect())
 
     entErns.unpersist()
     entRdd.unpersist()
-    //spark.stop()
+    lusRdd.unpersist()
+    losRdd.unpersist()
+    luErns.unpersist()
+    loErns.unpersist()
+
     res
+  }
+
+  private def getEntsFromEntTable(appconf: AppParams)(implicit ss:SparkSession) = {
+    HBaseDao.readEnterprisesWithKeyFilter(Configs.conf, appconf, ".*~" + {
+      appconf.TIME_PERIOD
+    } + "$")
+  }
+
+  private def getLocalUnitsFromLouTable(appconf: AppParams)(implicit ss:SparkSession) = {
+    HBaseDao.readLouWithKeyFilter(Configs.conf, appconf, ".*~" + {
+      appconf.TIME_PERIOD
+    } + "~.*")
+  }
+
+  private def getLegalUnitsFromLinks(appconf: AppParams)(implicit ss:SparkSession) = {
+    HBaseDao.readLinksWithKeyFilter(Configs.conf, appconf, ".*~LEU~" + {
+      appconf.TIME_PERIOD
+    } + "$")
+      .collect { case row if (row.cells.find(_.column == "p_ENT").isDefined) => row }
   }
 
   def getRepartionedRdd[T](rdd:RDD[T]) = {
     val noOfPartiions = rdd.getNumPartitions
     rdd.repartition(noOfPartiions)
+    rdd
   }
 
   def getChildlessEnts(entErns:RDD[String],luErns:RDD[String],loErns:RDD[String]) = {
     val luLessEnts = entErns.subtract(luErns)
     val loLessEnts = entErns.subtract(loErns)
-    loLessEnts.intersection(loLessEnts)
+    loLessEnts.intersection(luLessEnts)
   }
 
   def getOrphanLus(lus:RDD[HFileRow], orphanLuErns:RDD[String])(implicit spark: SparkSession) = {
@@ -88,4 +113,4 @@ object InputAnalyser extends RddLogging{
 
 
 
-  }
+}
