51aeb51d16cba2bb5fec9f58266f8f3396804ebc
ranjit-das <34281393+ranjit-das@users.noreply.github.com>
2018-06-11 14:53:38 +0100

    timeout params increased

--- src/main/scala/global/Configs.scala
+++ src/main/scala/global/Configs.scala
@@ -18,9 +18,7 @@ object Configs{
   val config: Config = ConfigFactory.load()
 
   val conf: Configuration = HBaseConfiguration.create()
-  conf.setInt("hbase.rpc.timeout", 300000)
-  //conf.setInt("hbase.client.scanner.timeout.period", 300000)
-  //conf.setInt("hbase.cells.scanned.per.heartbeat.check", 50000)
+ 
   Try{config.getString("hadoop.security.authentication")}.map(conf.addResource).getOrElse(conf.set("hadoop.security.authentication","kerberos"))
   Try{config.getString("hbase.security.authentication")}.map(conf.addResource).getOrElse(conf.set("hbase.security.authentication","kerberos"))
   Try{config.getString("hbase.kerberos.config")}.map(conf.addResource).getOrElse(logger.info("no config resource for kerberos specified"))
@@ -29,6 +27,11 @@ object Configs{
     conf.set("hbase.zookeeper.quorum", config.getString("hbase.zookeper.url"))
     conf.setInt("hbase.mapreduce.bulkload.max.hfiles.perRegion.perFamily", 500)
   }
+   //timeout params
+  conf.setInt("hbase.rpc.timeout", 360000)
+  conf.setInt("hbase.client.scanner.timeout.period", 360000)
+  conf.setInt("hbase.cells.scanned.per.heartbeat.check", 60000)
+  
   lazy val PATH_TO_JSON = config.getString("files.json")
 
 }
