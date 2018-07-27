1fe07bd73f951a1f27cfd86b29d9614c21d74035
vladshiligin <vladtoldmetodoit@hotmail.com>
2018-07-27 14:51:23 +0100

    sic07 error fix

--- src/main/scala/closures/NewPeriodWithCalculationsClosure.scala
+++ src/main/scala/closures/NewPeriodWithCalculationsClosure.scala
@@ -140,7 +140,12 @@ trait NewPeriodWithCalculationsClosure extends AdminDataCalculator with BaseClos
         Try {row.getAs[String]("address4")}.getOrElse (null),
         Try {row.getAs[String]("address5")}.getOrElse (null),
         row.getAs[String]("postcode"),
-        row.getAs[String]("sic07"),
+        //row.getAs[String]("sic07"),
+        Try{
+          val empl = row.getAs[String]("sic07")
+          empl.size //just to ensure Some(null) not returned
+          empl
+        }.getOrElse(""),
         Try{
           val empl = row.getAs[String]("paye_empees")
           empl.size //just to ensure Some(null) not returned
