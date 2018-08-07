5648f30642cbf9be47adc20362532407f828f225
vladshiligin <vladtoldmetodoit@hotmail.com>
2018-08-07 11:18:03 +0100

    commonly user methods factored out to BaseClosure

--- src/main/scala/closures/NewPeriodClosure.scala
+++ src/main/scala/closures/NewPeriodClosure.scala
@@ -1,14 +1,8 @@
 package closures
 
-import dao.hbase.{HBaseDao, HFileUtils}
+import dao.hbase.HFileUtils
 import global.{AppParams, Configs}
-import model.domain.HFileRow
-import model.hfile
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.KeyValue
-import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import spark.RddLogging
@@ -30,16 +24,12 @@ class NewPeriodClosure extends HFileUtils with BaseClosure with RddLogging with 
       **/
     val incomingBiDataDF: DataFrame = getIncomingBiData(appconf)
 
-    /*incomingBiDataDF.show()
-    incomingBiDataDF.printSchema()*/
 
     /**
       * id, ern
       **/
     val existingLEsDF: DataFrame = getExistingLeusDF(appconf, confs)
 
-    /*existingLEsDF.show()
-    existingLEsDF.printSchema()*/
     /**
       * Assigns ERNs to existing LEUs
       * leaving ERN field with null values for new LEUs
@@ -49,8 +39,6 @@ class NewPeriodClosure extends HFileUtils with BaseClosure with RddLogging with 
       **/
     val joinedLUs: DataFrame = incomingBiDataDF.join(existingLEsDF, Seq("id"), "left_outer")
     joinedLUs.cache()
-    /*joinedLUs.show()
-    joinedLUs.printSchema()*/
 
     /**
       * Generate ERNs for new BI Units
@@ -60,32 +48,25 @@ class NewPeriodClosure extends HFileUtils with BaseClosure with RddLogging with 
       **/
     val allLUsDF: DataFrame = getAllLUs(joinedLUs, appconf)
     allLUsDF.cache()
-    //allLUsDF.show()
+
     /**
       * ern, entref, name, trading_style, address1, address2, address3, address4, address5, postcode, sic07, legal_status
       **/
     val existingEntDF = getExistingEntsDF(appconf, confs)
-    //existingEntDF.show()
-/*    val existingEntCalculatedDF = existingEntDF.join(allLUsDF, Seq("ern"), "left_outer")
-    existingEntCalculatedDF.show()*/
+
     /**
       * ern, id, BusinessName, IndustryCode, LegalStatus, PostCode, TradingStatus Turnover, UPRN, CompanyNo, PayeRefs, VatRefs
       **/
     val newLEUsDF = allLUsDF.join(existingEntDF.select(col("ern")), Seq("ern"), "left_anti")
-    //newLEUsDF.show()
-/*    val newLEUsCalculatedDF = newLEUsDF.join(allLUsDF, Seq("ern"), "left_outer")
-    newLEUsCalculatedDF.show()*/
+
     /**
       * ern, entref, name, trading_style, address1, address2, address3, address4, address5, postcode, sic07, legal_status
       **/
     val newEntDF = spark.createDataFrame(createNewEntsWithoutCalculations(newLEUsDF).rdd, entRowSchema)
-    //newEntsCalculatedDF.show()
 
-    //val allEntsErns = allLUsDF.select(col("ern")).distinct
-    //allEntsErns.show()
 
     val allEntsDF = existingEntDF.union(newEntDF)
-    //allEntsDF.show()
+
     /**
       * Fields:
       * lurn, luref, ern, entref, name, tradingstyle, address1, address2, address3, address4, address5, postcode, sic07, employees
@@ -97,8 +78,6 @@ class NewPeriodClosure extends HFileUtils with BaseClosure with RddLogging with 
     saveEnts(allEntsDF, appconf)
     saveLous(allLOUs, appconf)
     saveLinks(allLOUs, allLUsDF, appconf)
-    //calculatedDF.show()
-    //calculatedDF.printSchema()
     allLUsDF.unpersist()
     joinedLUs.unpersist()
   }
@@ -107,32 +86,10 @@ class NewPeriodClosure extends HFileUtils with BaseClosure with RddLogging with 
 
     val existingLOUs: DataFrame = getExistingLousDF(appconf, confs)
     val entsWithoutLOUs: DataFrame = allEntsDF.join(existingLOUs.select("ern"), Seq("ern"), "left_anti")
-    val newAndMissingLOUsDF: DataFrame = createNewAndMissingLOUs(entsWithoutLOUs,appconf)
+    val newAndMissingLOUsDF: DataFrame = createNewLOUs(entsWithoutLOUs,appconf)
     val allLOUs = existingLOUs.union(newAndMissingLOUsDF)
 
     allLOUs
-  }
-
-  //ern, entref, name, trading_style, address1, address2, address3, address4, address5, postcode, sic07, legal_status
-  def createNewAndMissingLOUs(ents: DataFrame, appconf: AppParams)(implicit spark: SparkSession) = {
-
-    spark.createDataFrame(
-      ents.rdd.map(row => Row(
-        generateLurn(row,appconf),
-        Try {row.getAs[String]("luref")}.getOrElse(null),
-        row.getAs[String]("ern"),
-        row.getAs[String]("entref"),
-        row.getAs[String]("name"),
-        Try {row.getAs[String]("tradingstyle")}.getOrElse(null),
-        row.getAs[String]("address1"),
-        Try {row.getAs[String]("address2")}.getOrElse(null),
-        Try {row.getAs[String]("address3")}.getOrElse (null),
-        Try {row.getAs[String]("address4")}.getOrElse (null),
-        Try {row.getAs[String]("address5")}.getOrElse (null),
-        Try {row.getAs[String]("postcode")}.getOrElse (null),
-        row.getAs[String]("sic07"),
-        Try {row.getAs[String]("paye_empees")}.getOrElse("")
-      )), louRowSchema)
   }
 
   /**
@@ -182,39 +139,7 @@ class NewPeriodClosure extends HFileUtils with BaseClosure with RddLogging with 
     )
     ), entRowSchema)
 
-  /**
-    * Fields:
-    * id,BusinessName, UPRN, PostCode,IndustryCode,LegalStatus,
-    * TradingStatus, Turnover, EmploymentBands, PayeRefs, VatRefs, CompanyNo
-    **/
-  def getIncomingBiData(appconf: AppParams)(implicit spark: SparkSession) = {
-    val updatedConfs = appconf.copy(TIME_PERIOD = appconf.PREVIOUS_TIME_PERIOD)
-    val parquetDF = spark.read.parquet(appconf.PATH_TO_PARQUET)
-    parquetDF.castAllToString
-  }
 
-  def getAllLUs(joinedLUs: DataFrame, appconf: AppParams)(implicit spark: SparkSession) = {
-
-    val rows = joinedLUs.rdd.map { row => {
-      val ern = if (row.isNull("ern")) generateErn(row, appconf) else row.getAs[String]("ern")
-
-      Row(
-        ern,
-        row.getAs[String]("id"),
-        row.getAs[String]("BusinessName"),
-        row.getAs[String]("IndustryCode"),
-        row.getAs[String]("LegalStatus"),
-        row.getAs[String]("PostCode"),
-        row.getAs[String]("TradingStatus"),
-        row.getAs[String]("Turnover"),
-        row.getAs[String]("UPRN"),
-        row.getAs[String]("CompanyNo"),
-        row.getAs[Seq[String]]("PayeRefs"),
-        row.getAs[Seq[String]]("VatRefs")
-      )}}
-    //printRddOfRows("rows",rows)
-   spark.createDataFrame(rows, biWithErnSchema)
-  }
 }
 
 object NewPeriodClosure extends NewPeriodClosure
