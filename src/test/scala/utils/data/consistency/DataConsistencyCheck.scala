3eb0cc661798f78b0335937c0066ee4165b486e6
vladshiligin <vlad.shiligin@ons.gov.uk>
2018-09-03 09:04:52 +0100

    imports optimised

--- src/test/scala/utils/data/consistency/DataConsistencyCheck.scala
+++ src/test/scala/utils/data/consistency/DataConsistencyCheck.scala
@@ -1,14 +1,7 @@
 package utils.data.consistency
 
 import dao.hbase.HBaseConnectionManager
-import global.AppParams
 import model.domain._
-import org.apache.hadoop.hbase.client.Connection
-import org.apache.spark.sql.SparkSession
-import org.scalatest._
-import utils.Paths
-import utils.data.existing.ExistingData
-import utils.data.expected.ExpectedDataForAddNewPeriodScenario
 /**
   *
   */
