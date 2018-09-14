9a87b81c5348b07020a78360d3ce3f0419c524ab
vladshiligin <vladtoldmetodoit@hotmail.com>
2018-09-14 13:21:36 +0100

    debug log added temporarily

--- src/main/scala/closures/RefreshPeriodWithCalculationsClosure.scala
+++ src/main/scala/closures/RefreshPeriodWithCalculationsClosure.scala
@@ -197,6 +197,9 @@ trait RefreshPeriodWithCalculationsClosure extends AdminDataCalculator with Base
   def getAllLeus(allEntsDF:DataFrame, appconf: AppParams, confs:Configuration)(implicit spark: SparkSession) = {
 
     val existingLEUs: DataFrame = getExistingLeusDF(appconf,confs)
+    println("EXISTING LEU START******************************************")
+    existingLEUs.show(100)
+    println("EXISTING LEU END********************************************")
     val newLeusDF = spark.sql(s"""SELECT * FROM $newLeusViewName""")
     existingLEUs.union(newLeusDF)
   }
