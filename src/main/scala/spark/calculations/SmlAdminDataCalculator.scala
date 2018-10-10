3d6cace0f6b504e165b65bb5804d10f94510ef4e
vladshiligin <vladtoldmetodoit@hotmail.com>
2018-10-10 11:27:00 +0100

    1.0 version of registers-sml dependency added to build.sbt

--- src/main/scala/spark/calculations/SmlAdminDataCalculator.scala
+++ src/main/scala/spark/calculations/SmlAdminDataCalculator.scala
@@ -1,15 +1,16 @@
 package spark.calculations
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import temp.calculations.methods.{PayeCalculator, VatCalculator}
+//import temp.calculations.methods.{PayeCalculator, VatCalculator}
+import uk.gov.ons.registers.methods._
 
 trait SmlAdminDataCalculator extends PayeCalculator with VatCalculator{
 
   def calculate(unitsDF:DataFrame, payeDF:DataFrame, vatDF:DataFrame)(implicit spark: SparkSession ) = {
 
-    val payeCalculated:DataFrame = calculatePaye(unitsDF,payeDF)
+    val payeCalculated:DataFrame = calculatePAYE(unitsDF,payeDF)
 
-    calculateVat(unitsDF,payeCalculated,vatDF)
+    calculateVAT(unitsDF,payeCalculated,vatDF)
   }
 
 }
