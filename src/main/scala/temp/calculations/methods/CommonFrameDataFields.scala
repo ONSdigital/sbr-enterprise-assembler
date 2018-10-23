ba542ecdc7892869ad42633a5cd93625512b4199
vladshiligin <vlad.shiligin@ons.gov.uk>
2018-10-23 11:36:30 +0100

    schema refactored - names changed on BI records at the very start

---
+++ src/main/scala/temp/calculations/methods/CommonFrameDataFields.scala
@@ -0,0 +1,29 @@
+package temp.calculations.methods
+
+object CommonFrameDataFields {
+    val prn = "prn"
+    val cellNumber = "cell_no"
+    val payeEmployees = "paye_empees"
+    val sic07 = "sic07"
+
+    val employees = "paye_empees"
+    val jobs = "paye_jobs"
+    val ern = "ern"
+    val id = "ubrn"
+    val mar_jobs = "mar_jobs"
+    val june_jobs = "june_jobs"
+    val sept_jobs = "sept_jobs"
+    val dec_jobs = "dec_jobs"
+
+    //from paye input
+    val payeRefs = "payeref"
+
+    //from legal units input
+    val PayeRefs = "payerefs"
+
+    val contained = "cntd_turnover"
+    val apportioned = "app_turnover"
+    val standard = "std_turnover"
+    val group_turnover = "grp_turnover"
+    val ent = "ent_turnover"
+}
