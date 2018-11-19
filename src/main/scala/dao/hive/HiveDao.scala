81652cf593d825a40284063e4baa606344cc2066
ranjit-das <34281393+ranjit-das@users.noreply.github.com>
2018-11-19 15:45:35 +0000

    Update HiveDao.scala
    REG-2048: postcodeout column used for short post code

--- src/main/scala/dao/hive/HiveDao.scala
+++ src/main/scala/dao/hive/HiveDao.scala
@@ -10,7 +10,7 @@ trait HiveDao {
   def getRegions(appConf:AppParams)(implicit spark: SparkSession) =
     spark.sql(s"select postcode,gor as region from ${appConf.HIVE_DB_NAME}.${appConf.HIVE_TABLE_NAME}")
   def getRegionsShort(appConf:AppParams)(implicit spark: SparkSession) =
-    spark.sql(s"select postcode,gor as region from ${appConf.HIVE_DB_NAME}.${appConf.HIVE_SHORT_TABLE_NAME}")
+    spark.sql(s"select postcodeout,gor as region from ${appConf.HIVE_DB_NAME}.${appConf.HIVE_SHORT_TABLE_NAME}")
 
 }
 
