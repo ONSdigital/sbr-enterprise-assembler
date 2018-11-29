69d54783290f7f4c3f6df5857a08142da9d4b43b
vlad.shiligin <vlad.shiligin@ons.gov.uk>
2018-11-29 12:30:59 +0000

    Merge branch 'REG-2048_George' of https://github.com/ONSdigital/sbr-enterprise-assembler into 4merging-with-2048
    # Conflicts:
    #	src/main/resources/data/geo/test_short-dataset.csv
    #	src/main/scala/closures/AssembleUnitsClosure.scala
    #	src/main/scala/global/AppParams.scala
    #	src/main/scala/service/AddNewPeriodDataService.scala
    #	src/test/resources/data/geo/remove_last3_temp.sh
    #	src/test/resources/data/geo/test_short-dataset.csv
    #	src/test/scala/closures/AddNewPeriodWithCalculationsSpec.scala
    #	src/test/scala/utils/data/expected/ExpectedDataForAddNewPeriodScenario.scala

--- src/main/scala/dao/hive/HiveDao.scala
+++ src/main/scala/dao/hive/HiveDao.scala
@@ -13,8 +13,6 @@ trait HiveDao {
   def getRegionsShort(appConf:AppParams)(implicit spark: SparkSession) =
     spark.sql(s"select postcodeout,gor as region from ${appConf.HIVE_DB_NAME}.${appConf.HIVE_SHORT_TABLE_NAME}")
 
-  def getRegionsShort(appConf:AppParams)(implicit spark: SparkSession) =
-    spark.sql(s"select postcodeout,gor as region from ${appConf.HIVE_DB_NAME}.${appConf.HIVE_SHORT_TABLE_NAME}")
 
 }
 
