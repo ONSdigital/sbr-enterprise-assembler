280cb4bd2ae7c8380ddd1e4c5c468fa2e3f8624e
Paul Soule <paul.soule@ext.ons.gov.uk>
2019-01-03 10:42:26 +0000

    Added example application conf and updated readme

--- run_assembler.sh
+++ run_assembler.sh
@@ -2,6 +2,5 @@
 
 SPARK=~/spark/bin/spark-submit
 JAR=~/ONS/sbr-enterprise-assembler/target/scala-2.11/sbr-enterprise-assembler-assembly-1.1.jar
-DATA_DIR=~/ONS/sbr-enterprise-assembler/src/test/resources/data
 
 $SPARK --master local[*] --class assembler.AssemblerMain --files application.conf --conf spark.driver.extraJavaOptions=-Dconfig.file=application.conf $JAR
